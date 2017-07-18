026743e625a155658fda41d95cc4f5e486820c79
Csongor Pilinszki-Nagy <csongor.pilinszkinagy@gmail.com>
2017-07-18 21:19:58 +0200

    Working environment with random agent

---
+++ main.py
@@ -0,0 +1,33 @@
+import gym
+import random
+import numpy
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+from statistics import median, mean
+from collections import Counter
+
+# Define constants
+ENV_NAME = "CartPole-v0"
+LR = 1e-3
+MAX_STEPS = 500
+SCORE_REQUIREMENT = 50
+INITIAL_GAMES = 10000
+NUMBER_OF_EPISODES = 5
+
+# Initializing the environment
+env = gym.make(ENV_NAME)
+env.reset()
+
+# Random agent
+def randomAgent():
+    for episode in range(NUMBER_OF_EPISODES):
+        env.reset()
+        for t in range(MAX_STEPS):
+            env.render()
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            if done:
+                break
+
+randomAgent()
