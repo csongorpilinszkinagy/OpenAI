c1278173403c27795c7e747a5b6f118f50ead15f
Csongor Pilinszki-Nagy <csongor.pilinszkinagy@gmail.com>
2017-09-12 14:17:12 +0200

    Random Agent working

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 import gym
-import random
-import numpy
-import tflearn
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.estimator import regression
-from statistics import median, mean
-from collections import Counter
 
 # Define constants
 ENV_NAME = "CartPole-v0"
