cf19c2d6018cf2b424e13ba6809908ef14810295
Csongor Pilinszki-Nagy <csongor.pilinszkinagy@gmail.com>
2017-09-18 12:12:36 +0200

    Q learning copied working

--- main.py
+++ main.py
@@ -1,112 +1,184 @@
 import gym
+import random
+
+import numpy as np
 import tensorflow as tf
 
-LOGDIR = "/tmp/dqn/"
+class DQN:
+  REPLAY_MEMORY_SIZE = 10000
+  RANDOM_ACTION_DECAY = 0.99
+  MIN_RANDOM_ACTION_PROB = 0.1
+  HIDDEN1_SIZE = 20
+  HIDDEN2_SIZE = 20
+  NUM_EPISODES = 10000
+  MAX_STEPS = 1000
+  LEARNING_RATE = 0.001
+  MINIBATCH_SIZE = 30
+  DISCOUNT_FACTOR = 0.9
+  TARGET_UPDATE_FREQ = 300
+  REG_FACTOR = 0.001
+  LOG_DIR = '/tmp/dqn'
 
-# Define constants
-ENV_NAME = "CartPole-v0"
-MAX_STEPS = 500
-NUMBER_OF_EPISODES = 10
+  random_action_prob = 0.5
+  replay_memory = []
 
-class agent:
-    # Define constants
-    REPLAY_MEMORY_SIZE = 10000
-    EPSILON_MIN = 0.01
-    EPSILON_MAX = 1
-    LEARNING_RATE_MIN = 0.001
-    LEARNING_RATE_MAX = 0.01
-    HIDDEN_SIZE = 20
-    NUMBER_OF_EPISODES = 1000
-    MAX_STEPS = 500
-    MINIBATCH_SIZE = 30
-    DISCOUNT_FACTOR = 0.99
-    TARGET_UPDATE_FREQ= 500
-    STDDEV = 0.01
+  def __init__(self, env):
+    self.env = gym.make(env)
+    assert len(self.env.observation_space.shape) == 1
+    self.input_size = self.env.observation_space.shape[0]
+    self.output_size = self.env.action_space.n
+    
+  def init_network(self):
+    # Inference
+    self.x = tf.placeholder(tf.float32, [None, self.input_size])
+    with tf.name_scope('hidden1'):
+      W1 = tf.Variable(
+                 tf.truncated_normal([self.input_size, self.HIDDEN1_SIZE], 
+                 stddev=0.01), name='W1')
+      b1 = tf.Variable(tf.zeros(self.HIDDEN1_SIZE), name='b1')
+      h1 = tf.nn.tanh(tf.matmul(self.x, W1) + b1)
+    with tf.name_scope('hidden2'):
+      W2 = tf.Variable(
+                 tf.truncated_normal([self.HIDDEN1_SIZE, self.HIDDEN2_SIZE], 
+                 stddev=0.01), name='W2')
+      b2 = tf.Variable(tf.zeros(self.HIDDEN2_SIZE), name='b2')
+      h2 = tf.nn.tanh(tf.matmul(h1, W2) + b2)
+    with tf.name_scope('output'):
+      W3 = tf.Variable(
+                 tf.truncated_normal([self.HIDDEN2_SIZE, self.output_size], 
+                 stddev=0.01), name='W3')
+      b3 = tf.Variable(tf.zeros(self.output_size), name='b3')
+      self.Q = tf.matmul(h2, W3) + b3
+    self.weights = [W1, b1, W2, b2, W3, b3]
 
-    def __init__(self, environment):
-        self.environment = environment
-        self.input_size = self.environment.observation_space.shape[0]
-        self.output_size = self.environment.action_space.n
-        self.replay_memory = []
+    # Loss
+    self.targetQ = tf.placeholder(tf.float32, [None])
+    self.targetActionMask = tf.placeholder(tf.float32, [None, self.output_size])
+    # TODO: Optimize this
+    q_values = tf.reduce_sum(tf.multiply(self.Q, self.targetActionMask), 
+                  reduction_indices=[1])
+    self.loss = tf.reduce_mean(tf.square(tf.subtract(q_values, self.targetQ)))
 
-    def init_network(self):
-        
-        tf.reset_default_graph()
-        self.session = tf.Session()
+    # Reguralization
+    for w in [W1, W2, W3]:
+      self.loss += self.REG_FACTOR * tf.reduce_sum(tf.square(w))
 
-        #Setup placeholders
-        self.input = tf.placeholder(tf.float32, shape=[None, self.input_size], name="input")
-        self.targetQ = tf.placeholder(tf.float32, [None], name="target_q")
-        self.targetActionMask = tf.placeholder(tf.float32, [None, self.output_size], name="target_action_mask")
+    # Training
+    optimizer = tf.train.AdamOptimizer(self.LEARNING_RATE)
+    global_step = tf.Variable(0, name='global_step', trainable=False)
+    self.train_op = optimizer.minimize(self.loss, global_step=global_step)
 
-        hidden_layer = fc_relu_layer(self.input, self.input_size, self.HIDDEN_SIZE, name="hidden_layer")
-        self.q = fc_layer(hidden_layer, self.HIDDEN_SIZE, self.output_size, name="q")
-        
-        # TODO: Optimize this
-        with tf.name_scope("loss"):
-            q_values = tf.reduce_sum(tf.matmul(self.q, self.targetActionMask), reduction_indices=[1])
-            self.loss = tf.reduce_mean(tf.square(tf.subtract(q_values, self.targetQ)))
-            tf.summary.scalar("loss", loss)
+  def train(self, num_episodes=NUM_EPISODES):
+    self.session = tf.Session()
 
-        with tf.name_scope("train"):
-            self.train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+    # Summary for TensorBoard
+    tf.summary.scalar('loss', self.loss)
+    tf.summary.histogram('Q_values', self.Q)
+    self.summary = tf.summary.merge_all()
+    self.summary_writer = tf.summary.FileWriter(self.LOG_DIR, self.session.graph)
 
-        self.summary = tf.merge_all_summaries()
-        self.summary_writer = tf.summary.FileWriter(LOGDIR)
-        self.summary_writer.add_graph(sess.graph)
-        self.summary_writer.add_summary(self.summary)
+    self.session.run(tf.global_variables_initializer())
+    total_steps = 0
+    step_counts = []
 
-    # def play():
-        
+    target_weights = self.session.run(self.weights)
+    for episode in range(num_episodes):
+      state = self.env.reset()
+      steps = 0
 
-    #     # Initializing the environment
-    #     env = gym.make(ENV_NAME)
-    #     env.reset()
+      for step in range(self.MAX_STEPS):
+        # Pick the next action and execute it
+        action = None
+        if random.random() < self.random_action_prob:
+          action = self.env.action_space.sample()
+        else:
+          q_values = self.session.run(self.Q, feed_dict={self.x: [state]})
+          action = q_values.argmax()
+        self.update_random_action_prob()
+        obs, reward, done, _ = self.env.step(action)
 
-    #     for episode in range(NUMBER_OF_EPISODES):
-    #         env.reset()
-    #         for t in range(MAX_STEPS):
-    #             env.render()
-    #             action = env.action_space.sample()
-    #             observation, reward, done, info = env.step(action)
-    #             if done:
-    #                 break
+        # Update replay memory
+        if done:
+          reward = -100
+        self.replay_memory.append((state, action, reward, obs, done))
+        if len(self.replay_memory) > self.REPLAY_MEMORY_SIZE:
+          self.replay_memory.pop(0)
+        state = obs
 
-    def fc_layer(input, size_in, size_out, name="fc_layer"):
-        with tf.name_scope(name):
-            w = tf.Variable(tf.truncated_normal([size_in, size_out], stddev=0.1), name="w")
-            b = tf.Variable(tf.constant(0.1, shape=[size_out]), name="b")
-            act = tf.matmul(input, w) + b
-            tf.summary.histogram("weights", w)
-            tf.summary.histogram("biases", b)
-            tf.summary.histogram("activations", act)
-            return act
+        # Sample a random minibatch and fetch max Q at s'
+        if len(self.replay_memory) >= self.MINIBATCH_SIZE:
+          minibatch = random.sample(self.replay_memory, self.MINIBATCH_SIZE)
+          next_states = [m[3] for m in minibatch]
+          # TODO: Optimize to skip terminal states
+          feed_dict = {self.x: next_states}
+          feed_dict.update(zip(self.weights, target_weights))
+          q_values = self.session.run(self.Q, feed_dict=feed_dict)
+          max_q_values = q_values.max(axis=1)
 
-    def fc_relu_layer(input, size_in, size_out, name="fc_relu_layer"):
-        with tf.name_scope(name):
-            relu = tf.nn.relu(self.fc_layer(input, size_in, size_out))
-            tf.summary.histogram("relu", relu)
-            return relu
+          # Compute target Q values
+          target_q = np.zeros(self.MINIBATCH_SIZE)
+          target_action_mask = np.zeros((self.MINIBATCH_SIZE, self.output_size), dtype=int)
+          for i in range(self.MINIBATCH_SIZE):
+            _, action, reward, _, terminal = minibatch[i]
+            target_q[i] = reward
+            if not terminal:
+              target_q[i] += self.DISCOUNT_FACTOR * max_q_values[i]
+            target_action_mask[i][action] = 1
 
-    def random_steps(self):
-        state = self.environment.reset()
-        print self.output_size
-        for i in range(100):
-            action = self.environment.action_space.sample()
-            observation, reward, done, info = self.environment.step(action)
-            self.replay_memory.append((state, action, reward, observation, done))
-            state = observation
+          # Gradient descent
+          states = [m[0] for m in minibatch]
+          feed_dict = {
+            self.x: states, 
+            self.targetQ: target_q,
+            self.targetActionMask: target_action_mask,
+          }
+          _, summary = self.session.run([self.train_op, self.summary], 
+                                        feed_dict=feed_dict)
 
-            if done:
-                break
+          # Write summary for TensorBoard
+          if total_steps % 100 == 0:
+            self.summary_writer.add_summary(summary, total_steps)
 
-        print self.replay_memory
+        total_steps += 1
+        steps += 1
+        if done:
+          break
 
-def main():
-    environment = gym.make("CartPole-v0")
-    cartpole_agent = agent(environment)
-    cartpole_agent.random_steps()
+      step_counts.append(steps) 
+      mean_steps = np.mean(step_counts[-100:])
+      print("Training episode = {}, Total steps = {}, Last-100 mean steps = {}"
+                                      .format(episode, total_steps, mean_steps))
 
+      # Update target network
+      if episode % self.TARGET_UPDATE_FREQ == 0:
+        target_weights = self.session.run(self.weights)
 
-if __name__ == "__main__":
-    main()
+  def update_random_action_prob(self):
+    self.random_action_prob *= self.RANDOM_ACTION_DECAY
+    if self.random_action_prob < self.MIN_RANDOM_ACTION_PROB:
+      self.random_action_prob = self.MIN_RANDOM_ACTION_PROB
+
+  def play(self):
+    state = self.env.reset()
+    done = False
+    steps = 0
+    while not done and steps < 200:
+      self.env.render()
+      q_values = self.session.run(self.Q, feed_dict={self.x: [state]})
+      action = q_values.argmax()
+      state, _, done, _ = self.env.step(action)
+      steps += 1
+    return steps
+
+if __name__ == '__main__':
+  dqn = DQN('CartPole-v0')
+  dqn.init_network()
+
+  dqn.train()
+
+  res = []
+  for i in range(100):
+    steps = dqn.play()
+    print("Test steps = ", steps)
+    res.append(steps)
+  print("Mean steps = ", sum(res) / len(res))
