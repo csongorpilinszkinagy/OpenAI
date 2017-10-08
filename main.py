8deaa15ec1ae1ca58551cc46eb81b216f39d875d
Csongor Pilinszki-Nagy <csongor.pilinszkinagy@gmail.com>
2017-10-08 21:56:58 +0200

    Convolution network working on small sample

--- main.py
+++ main.py
@@ -4,30 +4,26 @@ import random
 import numpy as np
 import tensorflow as tf
 
+from PIL import Image
+
 REPLAY_MEMORY_SIZE = 1000000
 EPSILON_MIN = 0.1
 EPSILON_MAX = 1.0
-HIDDEN_SIZE_1 = 32
-HIDDEN_SIZE_2 = 32
-NUMBER_OF_EPISODES = 100000
-MAX_STEPS = 1000
-LEARNING_RATE = 0.00025
+NUMBER_OF_EPISODES = 10
+END_AT_TOTAL_STEPS = 2000000
+START_UPDATE_AT = 10000
+END_UPDATE_AT = 100000
+MAX_STEPS = 10000
+LEARNING_RATE = 0.001
 MINIBATCH_SIZE = 32
 DISCOUNT_FACTOR = 0.99
 TARGET_UPDATE_FREQ = 10000
-REG_FACTOR = 0.001
-LOG_DIR = "/tmp/dqn"
-DROPOUT = 0.9
-START_UPDATE_AT = 10000
-END_UPDATE_AT = 100000
-END_AT_TOTAL_STEPS = 2000000
+LOG_DIR = "/Users/pilinszki-nagycsongor/Developer/OpenAI/save/"
+
 MINIMUM_SAMPLE_SIZE = 10000
 
-# Create session
-session = tf.InteractiveSession()
-
 def weight_variable(shape):
-    return tf.Variable(tf.truncated_normal(shape, stddev=0.1))
+  return tf.Variable(tf.truncated_normal(shape))
 
 def bias_variable(shape):
   return tf.Variable(tf.constant(0.1, shape=shape))
@@ -57,7 +53,7 @@ def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
     with tf.name_scope("Wx_plus_b"):
       preactivate = tf.matmul(input_tensor, weights) + biases
       tf.summary.histogram("pre_activations", preactivate)
-    activations = act(preactivate, name="activation")
+    activations = tf.maximum(preactivate, 0.1 * preactivate)
     tf.summary.histogram("activations", activations)
     return activations, weights, biases
 
@@ -68,7 +64,16 @@ def update_epsilon(total_steps):
     return EPSILON_MIN
   return EPSILON_MAX + (EPSILON_MIN - EPSILON_MAX) / (END_UPDATE_AT - START_UPDATE_AT) * (total_steps - START_UPDATE_AT)
 
+def format_image(image):
+  gray_image = tf.image.rgb_to_grayscale(image)
+  resized_image = tf.image.resize_images(gray_image, [84, 84], method=tf.image.ResizeMethod.AREA)
+  image_2d = np.squeeze(resized_image.eval(), axis=2)
+  return image_2d
+
 def train():
+  # Create session
+  session = tf.InteractiveSession()
+
   # TODO: Load if there is a model present
 
   # Create environment
@@ -76,68 +81,87 @@ def train():
   input_size = environment.observation_space.shape[0]
   output_size = environment.action_space.n
 
-  # Create model
-  state = tf.placeholder(tf.float32, [None, 84, 84, 4], name="state")
+  # TODO: parametrize sizes
+  with tf.name_scope("input"):
+    input_state = tf.placeholder(tf.float32, [None, 84, 84, 4], name="input_state")
+    tf.summary.histogram("input_state", input_state)
+    targetQ = tf.placeholder(tf.float32, [None], name="targetQ")
+    targetActionMask = tf.placeholder(tf.float32, [None, output_size], name="targetActionMask")
 
   with tf.name_scope("conv1"):
-    W_conv1 = weight_variable([8, 8, 4, 32])
+    W_conv1 = tf.get_variable("W_conv1", shape=[8, 8, 4, 32], initializer=tf.contrib.layers.xavier_initializer_conv2d(uniform=False))
     b_conv1 = bias_variable([32])
-    h_conv1 = tf.nn.relu(conv2d(state, W_conv1, 4) + b_conv1)
+    m_conv1 = conv2d(input_state, W_conv1, 4) + b_conv1
+    h_conv1 = tf.maximum(m_conv1, 0.1 * m_conv1)
+
+    tf.summary.histogram("W_conv1", W_conv1)
+    tf.summary.histogram("b_conv1", b_conv1)
+    tf.summary.histogram("m_conv1", m_conv1)
+    tf.summary.histogram("h_conv1", h_conv1)
   
   with tf.name_scope("conv2"):
-    W_conv2 = weight_variable([4, 4, 32, 64])
+    W_conv2 = tf.get_variable("W_conv2", shape=[4, 4, 32, 64], initializer=tf.contrib.layers.xavier_initializer_conv2d(uniform=False))
     b_conv2 = bias_variable([64])
-    h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2, 2) + b_conv2)
+    m_conv2 = conv2d(h_conv1, W_conv2, 2) + b_conv2
+    h_conv2 = tf.maximum(m_conv2, 0.1 * m_conv2)
+
+    tf.summary.histogram("W_conv2", W_conv2)
+    tf.summary.histogram("b_conv2", b_conv2)
+    tf.summary.histogram("m_conv2", m_conv2)
+    tf.summary.histogram("h_conv2", h_conv2)
   
   with tf.name_scope("conv3"):
-    W_conv3 = weight_variable([3, 3, 64, 64])
+    W_conv3 = tf.get_variable("W_conv3", shape=[3, 3, 64, 64], initializer=tf.contrib.layers.xavier_initializer_conv2d(uniform=False))
     b_conv3 = bias_variable([64])
-    h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
+    m_conv3 = conv2d(h_conv2, W_conv3, 1) + b_conv3
+    h_conv3 = tf.maximum(m_conv3, 0.1 * m_conv3)
+
+    tf.summary.histogram("W_conv3", W_conv3)
+    tf.summary.histogram("b_conv3", b_conv3)
+    tf.summary.histogram("m_conv3", m_conv3)
+    tf.summary.histogram("h_conv3", h_conv3)
 
   with tf.name_scope("flatten"):
     flat_conv3 = tf.reshape(h_conv3, [-1, 7744])
 
-  hidden1, W1, b1 = nn_layer(flat_conv3, 7744, HIDDEN_SIZE_1, "hidden1")
-  hidden2, W2, b2 = nn_layer(hidden1, HIDDEN_SIZE_1, HIDDEN_SIZE_2, "hidden2")
-  Q, W3, b3 = nn_layer(hidden2, HIDDEN_SIZE_2, output_size, "output_q", act=tf.identity)
-  weights = [W1, b1, W2, b2, W3, b3]
-
-  s = environment.reset()
-  s = tf.image.resize_images(s, [84, 84]).eval()
-
+  hidden, W1, b1 = nn_layer(flat_conv3, 7744, 512, "hidden")
+  Q, W2, b2 = nn_layer(hidden, 512, output_size, "output_q", act=tf.identity)
+  weights = [W_conv1, b_conv1, W_conv2, b_conv2, W_conv3, b_conv3, W1, b1, W2, b2]
   
   with tf.name_scope("Q_learning"):
-    targetQ = tf.placeholder(tf.float32, [None], name="targetQ")
-    tf.summary.scalar("targetQ", targetQ)
-
-    targetActionMask = tf.placeholder(tf.float32, [None, output_size], name="targetActionMask")
     maskedQ = tf.reduce_sum(tf.multiply(Q, targetActionMask), reduction_indices=[1], name="maskedQ")
-    tf.summary.scalar("maskedQ", maskedQ)
     loss = tf.reduce_mean(tf.square(tf.subtract(maskedQ, targetQ)), name="loss")
 
-  optimizer = tf.train.AdamOptimizer(LEARNING_RATE)
-  train_op = optimizer.minimize(loss)
+    tf.summary.scalar("loss", loss)
+
+  with tf.name_scope("train"):
+    train_op = tf.train.AdamOptimizer(LEARNING_RATE).minimize(loss)
 
   # Merge summaries
-  merged = tf.summary.merge_all()
+  merged_summary = tf.summary.merge_all()
   tf.global_variables_initializer().run()
 
-  # Write summary
+  # Summary writer and model saver
   summary_writer = tf.summary.FileWriter(LOG_DIR, session.graph)
+  model_saver = tf.train.Saver()
+
+  # TODO: if there is a model load it
+  # model_saver.restore(session, LOG_DIR + "model.ckpt")
 
   epsilon = EPSILON_MAX
   replay_memory = []
 
-  model_saver = tf.train.Saver()
-
   total_steps = 0
-
-  target_weights = session.run(weights)
+  fixed_weights = session.run(weights)
   
   # Run episodes
   for episode in range(NUMBER_OF_EPISODES):
     # Reset environment
-    state = environment.reset()
+    image = environment.reset()
+    small_image =  format_image(image)
+    normalized_image = (small_image - 128.0) / 128.0
+    image_array = [normalized_image, normalized_image, normalized_image, normalized_image]
+    state = np.stack(image_array, axis=2)
     # Take steps
     for step in range(MAX_STEPS):
       # Pick the next action and execute it
@@ -149,68 +173,66 @@ def train():
         q_values = session.run(Q, feed_dict={input_state: [state]})
         action = q_values.argmax()
 
-      # Observation
-      observation, reward, done, _ = environment.step(action)
+      image, reward, done, _ = environment.step(action)
+      small_image = format_image(image)
+      normalized_image = (small_image - 128.0) / 128.0
+
+      image_array.pop(0)
+      image_array.append(normalized_image)
+      observation = np.stack(image_array, axis=2)
+
       total_steps += 1
+      print total_steps
 
       # Update replay memory
-      replay_memory.append((state, action, reward, observation, done))
+      if total_steps < 50:
+        replay_memory.append((state, action, reward, observation, done))
       if total_steps > REPLAY_MEMORY_SIZE:
         replay_memory.pop(0)
       
       state = observation
 
       # Sample a random minibatch
-      if total_steps > MINIMUM_SAMPLE_SIZE:
+      if total_steps > MINIBATCH_SIZE:
         minibatch = random.sample(replay_memory, MINIBATCH_SIZE)
         next_states = [m[3] for m in minibatch]
-        feed_dict = {state: next_states}
-        feed_dict.update(zip(weights, target_weights))
+        feed_dict = {input_state: next_states}
+        feed_dict.update(zip(weights, fixed_weights))
         q_values = session.run(Q, feed_dict=feed_dict)
         max_q_values = q_values.max(axis=1)
 
-#         # Sample a random minibatch and fetch max Q at s'
-#         if len(self.replay_memory) >= 10000:
-#           minibatch = random.sample(self.replay_memory, self.MINIBATCH_SIZE)
-#           next_states = [m[3] for m in minibatch]
-#           # TODO: Optimize to skip terminal states
-#           feed_dict = {self.x: next_states}
-#           feed_dict.update(zip(self.weights, target_weights))
-#           q_values = self.session.run(self.Q, feed_dict=feed_dict)
-#           max_q_values = q_values.max(axis=1)
+        # Compute target Q values
+        # TODO: Vectorize computations
+        target_q = np.zeros(MINIBATCH_SIZE)
+        target_action_mask = np.zeros((MINIBATCH_SIZE, output_size), dtype=int)
+        for i in range(MINIBATCH_SIZE):
+          _, action, reward, _, terminal = minibatch[i]
+          target_q[i] = reward
+          # TODO: uncomment
+          if not terminal:
+            target_q[i] += DISCOUNT_FACTOR * max_q_values[i]
+          target_action_mask[i][action] = 1
 
-#           # Compute target Q values
-#           target_q = np.zeros(self.MINIBATCH_SIZE)
-#           target_action_mask = np.zeros((self.MINIBATCH_SIZE, self.output_size), dtype=int)
-#           for i in range(self.MINIBATCH_SIZE):
-#             _, action, reward, _, terminal = minibatch[i]
-#             target_q[i] = reward
-#             if not terminal:
-#               target_q[i] += self.DISCOUNT_FACTOR * max_q_values[i]
-#             target_action_mask[i][action] = 1
+        # Gradient descent
+        states = [m[0] for m in minibatch]
+        feed_dict = {
+          input_state: states, 
+          targetQ: target_q,
+          targetActionMask: target_action_mask,
+        }
+        _, summary = session.run([train_op, merged_summary], 
+            feed_dict=feed_dict)
 
-#           # Gradient descent
-#           states = [m[0] for m in minibatch]
-#           feed_dict = {
-#             self.x: states, 
-#             self.targetQ: target_q,
-#             self.targetActionMask: target_action_mask,
-#           }
-#           _, summary = self.session.run([self.train_op, self.summary], 
-#               feed_dict=feed_dict)
+        # Write summary for TensorBoard
+        if total_steps % 1 == 0:
+          summary_writer.add_summary(summary, total_steps)
 
-#           # Write summary for TensorBoard
-#           if total_steps % 1000 == 0:
-#             self.summary_writer.add_summary(summary, total_steps)
+        # Update target network
+        if step % 1000 == 0:
+          fixed_weights = session.run(weights)
 
-#           # Update target network
-#           if step % self.TARGET_UPDATE_FREQ == 0:
-#             target_weights = self.session.run(self.weights)
-
-#         total_steps += 1
-#         steps += 1
-#         if done:
-#           break
+      if done:
+        break
 
 #       step_counts.append(steps) 
 #       mean_steps = np.mean(step_counts[-100:])
@@ -223,17 +245,19 @@ def train():
 
 #   
 
-#   def play(self):
-#     state = self.env.reset()
-#     done = False
-#     steps = 0
-#     while not done and steps < 1000:
-#       self.env.render()
-#       q_values = self.session.run(self.Q, feed_dict={self.x: [state]})
-#       action = q_values.argmax()
-#       state, _, done, _ = self.env.step(action)
-#       steps += 1
-#     return steps
+def play_random():
+  env = gym.make("Breakout-v0")
+  state = env.reset()
+  done = False
+  steps = 0
+  for i in range(1000):
+    env.render()
+    _, _, done, _ = env.step(env.action_space.sample())
+    if done:
+      break
+    time.sleep(1)
+    
+  return steps
 
 if __name__ == "__main__":
   train()
